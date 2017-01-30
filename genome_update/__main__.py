167feaeaecdfd5e8926ff320762969acf552844f
Emil Samuelsson <emil@samuelsson.pp.se>
2017-01-30 14:14:02 +0100

    Changed a command argument for clearification

--- genome_update/__main__.py
+++ genome_update/__main__.py
@@ -10,14 +10,14 @@ def main():
 	SUPPORTED_DOMAINS = ['archaea', 'bacteria', 'fungi', 'invertebrate', 'plant', 'protozoa', 'vertebrate_mammalian', 'vertebrate_other', 'viral']
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('-f', '--folder-output', 
-						dest = 'folder',
-						default = 'genomes/',
-						help = 'The folder which files will be downloaded to')
-	parser.add_argument('-o', '--output-file', 
+	parser.add_argument('-name', '--newname', 
+						dest = 'name',
+						default = 'genome.yaml',
+						help = 'What the yaml file should be named')
+	parser.add_argument('-o', '--output',
 						dest = 'output', 
-						default = 'genomes.yaml', 
-						help = 'The folder which files will be downloaded to')
+						default = 'genomes/', 
+						help = 'The output which files will be downloaded to')
 	parser.add_argument('-p', '--parallel', 
 						dest = 'parallel', 
 						default = 1, 
@@ -62,25 +62,25 @@ def main():
 	NCBI = 'ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/' + args.domain + '/assembly_summary.txt'
 	yaml = yh.yaml_handler()
 
-	if not os.path.exists(args.folder):
-		os.makedirs(args.folder)
+	if not os.path.exists(args.output):
+		os.makedirs(args.output)
 
 	if args.download and args.update:
 		print('Both --download and --update-yaml cannot be supplied')
 	elif args.download:
 		if args.input != None and os.path.exists(args.input):
-			download(yaml, args.input, args.output, args.folder, args.parallel)
+			download(yaml, args.input, args.name, args.output, args.parallel)
 		else:
 			print('To use --download, specify a correct input with -i')
 	elif args.update:
 		if args.input != None and args.genus and os.path.exists(args.input):
 			print('Checking if input is uptodate with NCBI assembly')
-			update_yaml(yaml, NCBI, args.genus, args.species_taxid, args.taxid, args.output, args.input)
+			update_yaml(yaml, NCBI, args.genus, args.species_taxid, args.taxid, args.name, args.input)
 		else:
 			print('To use --download, specify a correct input with -i')
 	elif args.genus:
-		update_yaml(yaml, NCBI, args.genus, args.species_taxid, args.taxid, args.output, args.input)
-		download(yaml, args.output, args.output, args.folder, args.parallel)
+		update_yaml(yaml, NCBI, args.genus, args.species_taxid, args.taxid, args.name, args.input)
+		download(yaml, args.name, args.name, args.output, args.parallel)
 	else:
 		print('Input arguments was invalid, try -h for help')
 def download(yaml, inputfile, outputfile, directory, parallel):
